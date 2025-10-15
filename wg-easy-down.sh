7d6674415f95a02713f79b02cf5a6cc391645604
VPS <vps@vps.kutak>
2025-10-15 21:31:54 +0000

    ftp

--- wg-easy-down.sh
+++ wg-easy-down.sh
@@ -1,1 +1,2 @@
 docker compose -f wg-easy-docker-compose.yml down
+docker network remove vpn
--- wg-easy.sh
+++ wg-easy.sh
@@ -1,1 +1,8 @@
+docker network create \
+  --driver bridge \
+  --ipv6 \
+  --subnet 10.42.42.0/24 \
+  --subnet fdcc:ad94:bacf:61a3::/64 \
+  vpn
+
 docker compose -f wg-easy-docker-compose.yml up -d
