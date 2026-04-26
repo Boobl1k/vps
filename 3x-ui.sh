bd218028087d63decfcd1b06f5ed4596f0c6ecfc
Mansur <ahmatowmansur@gmail.com>
2026-04-26 13:55:23 +0300

    update 3x-ui

--- 3x-ui.sh
+++ 3x-ui.sh
@@ -1,1 +1,12 @@
+echo "Server IP: ${SERVER_IP}"
+
+mkdir -p ./cert
+
+sudo openssl req -x509 -newkey rsa:2048 \
+  -keyout ./key.pem \
+  -out ./cert.pem \
+  -days 365 \
+  -nodes \
+  -subj "/C=/ST=/L=/O=/OU=/CN=${SERVER_IP}"
+
 docker compose -f 3x-ui-docker-compose.yml up -d
---
+++ 3x-ui/3x-ui-readme.txt
@@ -0,0 +1,4 @@
+1. cp .env.example .env
+2. difine SERVER_IP and 3X_UI_ADMIN_PASS
+3. ./3x-ui.sh
+4. open https://<server_ip>:1011 in your browser to access panel
