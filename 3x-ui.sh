6ee6852215d3617dc5bc8a416f4ad5e410dbff21
Mansur <ahmatowmansur@gmail.com>
2026-04-26 14:11:59 +0300

    update 3x-ui

--- 3x-ui.sh
+++ 3x-ui.sh
@@ -7,8 +7,8 @@ echo "Server IP: ${SERVER_IP}"
 mkdir -p ./cert
 
 sudo openssl req -x509 -newkey rsa:2048 \
-  -keyout ./key.pem \
-  -out ./cert.pem \
+  -keyout ./cert/key.pem \
+  -out ./cert/cert.pem \
   -days 365 \
   -nodes \
   -subj "/C=/ST=/L=/O=/OU=/CN=${SERVER_IP}"
