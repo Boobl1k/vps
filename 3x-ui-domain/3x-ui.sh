e38a60079eada7c7707d6d2356261df71ae64c7f
Mansur <ahmatowmansur@gmail.com>
2026-04-28 16:50:48 +0300

    3x-ui with domain

--- 3x-ui-domain/3x-ui.sh
+++ 3x-ui-domain/3x-ui.sh
@@ -6,15 +6,7 @@ echo "Dir: ${DIR}"
 echo "Server IP: ${SERVER_IP}"
 echo "Server domain: ${SERVER_DOMAIN}"
 
-mkdir -p ./cert
-
-sudo openssl req -x509 -newkey rsa:2048 \
-  -keyout ./cert/key.key \
-  -out ./cert/cert.crt \
-  -days 365 \
-  -nodes \
-  -subj "/CN=${SERVER_IP}" \
-  -addext "subjectAltName = IP:${SERVER_IP} "
+./cert.sh
 
 mkdir -p ./3x_db
 sudo cp ./x-ui.db.original ./3x_db/x-ui.db
