ede69bd79428d6c5cdbe7259e725ab17f293f25d
Mansur <ahmatowmansur@gmail.com>
2026-04-28 19:14:44 +0300

    3x-ui add nginx

--- 3x-ui-domain/3x-ui.sh
+++ 3x-ui-domain/3x-ui.sh
@@ -2,20 +2,11 @@ set -a
 source .env
 set +a
 
-ADMIN_DOMAIN=admin.${SERVER_DOMAIN}
-SUBSCRIPTION_DOMAIN=sub.${SERVER_DOMAIN}
-
 echo "Server IP: ${SERVER_IP}"
 echo "Server domain: ${SERVER_DOMAIN}"
 echo "Admin domain: ${ADMIN_DOMAIN}"
 echo "Subscription domain: ${SUBSCRIPTION_DOMAIN}"
 
 ./cert.sh
-
-mkdir -p ./3x_db
-sudo cp ./x-ui.db.original ./3x_db/x-ui.db
-sudo chmod 644 ./3x_db/x-ui.db
-sudo chown root:root ./3x_db/x-ui.db
-sudo chown root:root ./3x_db/
 
 docker compose up -d
--- 3x-ui-domain/readme.txt
+++ 3x-ui-domain/readme.txt
@@ -1,5 +1,5 @@
 1. cp .env.example .env
-2. difine SERVER_IP and SERVER_DOMAIN
+2. difine SERVER_DOMAIN
 3. ./3x-ui.sh
 4. open https://<server_domain>:1011 in your browser to access panel. Default creds: admin admin
 5. update admin password!!!
